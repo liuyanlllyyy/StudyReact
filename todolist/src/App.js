8c63f1b221f2aef766dc45771b58a0b2a80cdb02
Yanni <liuyan@78dk.com>
2018-07-25 21:44:53 +0800

    React加上 antd 重新写 todolist

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,5 +1,5 @@
 import React ,{Component,Fragment}from 'react';
-import {CSSTransition} from 'react-transition-group';
+import {CSSTransition,TransitionGroup} from 'react-transition-group';
 import  './style.css';
 class App extends  Component{
 
