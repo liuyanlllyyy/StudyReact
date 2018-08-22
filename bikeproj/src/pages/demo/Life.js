8e139206583df6d35cfe5e685540be21cee9d105
Yanni <liuyan@78dk.com>
2018-08-22 14:11:48 +0800

    集成 antd

---
+++ bikeproj/src/pages/demo/Life.js
@@ -0,0 +1,18 @@
+ import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Life extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+Life.propTypes = {
+
+};
+
+export default Life;
