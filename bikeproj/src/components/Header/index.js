a402bfa4d7d1def99f564fc105b18a7917222a0e
Yanni <liuyan@78dk.com>
2018-08-26 09:52:56 +0800

    添加 jsonp 文件

--- bikeproj/src/components/Header/index.js
+++ bikeproj/src/components/Header/index.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import {Row,Col,Button} from 'antd';
 import './index.less';
 import Util from '../../utils/utils'
+import axios from '../../axios'
 export default class Header extends Component {
     state={}
     componentWillMount(){
         this.setState({
-            userName:"guofsu "
+            userName:"大帅比刘衍 "
         })
         setInterval(()=>{
            let sysTime = Util.fromatDate(new Date().getTime());
@@ -15,21 +16,45 @@ export default class Header extends Component {
                sysTime
            })
         },1000)
+        this.getWeatherApiData();
+
     }
+    getWeatherApiData(){
+        this.setState({
+            dayPictureUrl:"/assets/logo-ant.svg",
+            weather:"22测试度"
+        })
+        // let city = '成都';
+        // axios.jsonp({
+        //     url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=9GZY7puONUsbHqiuaEEYqYlVaYhNFtA0'
+        // }).then((res)=>{
+        //     if(res.status === 'success'){
+        //         let data = res.results[0].weather_data[0];
+        //         this.setState({
+        //             dayPictureUrl:data.dayPictureUrl,
+        //             weather:data.weather
+        //         })
+        //     }
+        // })
+    }
+
     render() {
         return (
             <div className= "header">
                 <Row className= "header-top">
                     <Col span="24">
                         <span> 欢迎,{this.state.userName}</span>
-                        <Button>退出是</Button>
+                        <Button type="primary">退出</Button>
                     </Col>
                 </Row>
                 <Row className="breadcrumb">
                     <Col span="4" className = "breadcrumb-title">首页</Col>
                     <Col span="20"className="weather">
                         <span className="date">{this.state.sysTime}</span>
-                        <span className="weather-detail"> 天气</span>
+                        <span className="weather-detail"> 
+                        {/* <img src={this.state.dayPictureUrl} alt=""></img> */}
+                        {this.state.weather}
+                        </span>
                     
                     </Col>
                 </Row>
