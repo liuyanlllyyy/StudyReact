67ae7f1a8e14cce24bbae851579cb4b4873f1b57
liuyan <liuyan@78dk.com>
2018-08-26 17:41:59 +0800

    新的imac测试

--- bikeproj/src/utils/utils.js
+++ bikeproj/src/utils/utils.js
@@ -2,7 +2,7 @@ export default {
     fromatDate(time){
         if(!time)return "";
         let date = new Date(time);
-        //update xiugai
+        //update xiugais 
         return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
     }
 }
